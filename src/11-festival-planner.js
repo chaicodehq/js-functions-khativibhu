b60b3dc774b424f9049e2261a16a0e782d2bf4b3
Vaibhav Khati <khativibhu@gmail.com>
2026-03-18 13:17:57 +0530

    11 done

--- src/11-festival-planner.js
+++ src/11-festival-planner.js
@@ -49,5 +49,73 @@
  *   mgr.getUpcoming("2025-01-01", 1); // => [{ name: "Republic Day", ... }]
  */
 export function createFestivalManager() {
-  // Your code here
+  let festivals=[];  //private state
+
+  return {
+    addFestival(name,date,type){
+       if(name.trim().length === 0 || typeof date != 'string' || (type !== "religious" && type !== "national" && type !== "cultural"))
+       {
+        return -1;
+       }
+
+       for(let i=0;i<festivals.length;i++)
+       {
+         if(festivals[i].name === name)
+         {
+          return -1;  //addFestival() returns -1 if duplicate name is given as argument.
+         }
+       }
+       
+      return festivals.push({name,date,type});
+    },
+
+    removeFestival: function (name){
+      const indexToRemove = festivals.findIndex((o)=> o.name === name);
+
+      if(indexToRemove > -1)
+      {
+      festivals.splice(indexToRemove,1); //remove 1 item at indexToRemove index
+      return true;
+      }
+
+      return false;
+    },
+
+    getAll: function(){
+      const deepCopy = [...festivals];
+      return deepCopy; 
+    },
+
+    getByType: function(type){
+      return festivals.filter((festival)=> festival.type === type);
+    },
+
+    getUpcoming: function(currentDate,n=3) {
+       const filteredFestivals = festivals.filter( (festival)=>{
+         const d = new Date(festival.date);
+         const currentD = new Date(currentDate);
+
+         if(d >= currentD)
+         {
+          return true;
+         }
+         else{
+          return false;
+         }
+    });
+       
+      const sortedFestivals = filteredFestivals.sort((a,b)=> {
+       const aDate = new Date(a.date);
+       const bDate = new Date(b.date);
+
+       return aDate - bDate;   //-1 or 0 or +1 value is returned by this callback to sort() function. sort() calls this callback many times and recieves returned value. Then, sort() decides order, based on that value.
+      }); //sorted by date ascending 
+
+      return sortedFestivals.slice(0,n); //next n festivals
+    },
+
+    getCount: function(){
+      return festivals.length;
+    },
+  };
 }
